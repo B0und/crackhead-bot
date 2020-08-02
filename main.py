bd92ffe3363b8e8c3cd8cda0abf765727a6580b4
Vlad <f1arenova1@gmail.com>
2020-08-03 00:56:28 +0300

    opus fix v2

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ delay_duration = {"a1.mp3": 0.6319047619047619,
                   }
 
 
-discord.opus.load_opus()
+discord.opus.load_opus("opus")
 if not discord.opus.is_loaded():
     raise RuntimeError('Opus failed to load')
 
