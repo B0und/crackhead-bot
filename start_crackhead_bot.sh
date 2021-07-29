a8d68a3e1d9ef2cf0bf7c4053162f54a0c2bb8b2
Unbound <f1arenova1@gmail.com>
2021-07-29 21:43:18 +0300

    Update start_crackhead_bot.sh

--- start_crackhead_bot.sh
+++ start_crackhead_bot.sh
@@ -3,6 +3,7 @@
 # @reboot ~/crackhead-bot/start_crackhead_bot.sh >> ~/crackhead-bot/logs.txt 2>&1
 #* * * * *  /bin/sh -c 'cd ~/crackhead-bot; git pull -q' >> ~/crackhead-bot/logs.txt 2>&1
 
+sleep 10
 cd ~/crackhead-bot/
 source ~/crackhead-bot/venv/bin/activate
 python main.py
